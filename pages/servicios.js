0f2ba9cfd9823b9e6ada48822e7cdd42046a1ef5
Christian Villegas <soliscvs@gmail.com>
2022-07-14 21:40:51 -0500

    02 correcion_SSP

--- pages/servicios.js
+++ pages/servicios.js
@@ -1,32 +1,26 @@
-import Layout from "../components/Layout"
-import ListadoServicio from "../components/ListadoServicio"
+import Layout from "../components/Layout";
+import ListadoServicio from "../components/ListadoServicio";
 
-const Servicios = ({datos}) => {
-
+const Servicios = ({ datos }) => {
   return (
-    
     <Layout pagina="Servicios">
       <main className="contenedor">
-        <ListadoServicio
-          datos={datos}
-        />
+        <ListadoServicio datos={datos} />
       </main>
     </Layout>
+  );
+};
 
-  )
-}
-
-export const getServerSideProps = async () => {
-
-  const url = `${process.env.API_URL}/servicios`
-  const response = await fetch(url)
-  const datos = await response.json()
+export async function getServerSideProps() {
+  const url = `${process.env.API_URL}/servicios`;
+  const response = await fetch(url);
+  const datos = await response.json();
 
   return {
-    props:{
-      datos
-    }
-  }
+    props: {
+      datos,
+    },
+  };
 }
 
-export default Servicios
+export default Servicios;
